78ab98c7c4498d1c28492d2895b2802911ba767d
조윤성 <yunsung1206@gmail.com>
2026-01-06 14:17:12 +0900

    chore: PostGIS 환경 구성 및 hibernate-spatial 의존성 추가

--- src/test/java/waypoint/mvp/global/config/TestContainersConfig.java
+++ src/test/java/waypoint/mvp/global/config/TestContainersConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainersConfig {
@@ -11,6 +12,7 @@ public class TestContainersConfig {
 	@Bean
 	@ServiceConnection
 	public PostgreSQLContainer<?> postgreSQLContainer() {
-		return new PostgreSQLContainer<>("postgres:17.7");
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgis/postgis:17-3.5")
+			.asCompatibleSubstituteFor("postgres"));
 	}
 }
