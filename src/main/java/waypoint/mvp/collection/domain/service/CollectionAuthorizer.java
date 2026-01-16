32b660df527899bc6eafe4b2642fa02b2ff194df
Cori1304-Seong <andyjm7739@gmail.com>
2026-01-17 02:41:56 +0900

    feat(collection): CollectionAuthorizer 추가 [WAPO-67]
    - 사용자가 컬렉션에 속한 사람인지 확인하기 위해 사용됨

---
+++ src/main/java/waypoint/mvp/collection/domain/service/CollectionAuthorizer.java
@@ -0,0 +1,35 @@
+package waypoint.mvp.collection.domain.service;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import waypoint.mvp.collection.domain.Collection;
+import waypoint.mvp.collection.domain.CollectionRole;
+import waypoint.mvp.collection.error.CollectionError;
+import waypoint.mvp.collection.infrastructure.persistence.CollectionMemberRepository;
+import waypoint.mvp.global.error.exception.BusinessException;
+import waypoint.mvp.user.domain.User;
+
+@Component
+@RequiredArgsConstructor
+public class CollectionAuthorizer {
+
+	private final CollectionMemberRepository memberRepository;
+
+	public void verifyOwner(Collection collection, User user) {
+		memberRepository.findByCollectionAndUser(collection, user)
+			.filter(member -> member.getRole() == CollectionRole.OWNER)
+			.orElseThrow(() -> new BusinessException(CollectionError.FORBIDDEN_NOT_OWNER));
+	}
+
+	public void verifyMember(Collection collection, User user) {
+		memberRepository.findByCollectionAndUser(collection, user)
+			.orElseThrow(() -> new BusinessException(CollectionError.FORBIDDEN_NOT_MEMBER));
+	}
+
+	public void checkIfMemberExists(Collection collection, User user) {
+		memberRepository.findByCollectionAndUser(collection, user).ifPresent(member -> {
+			throw new BusinessException(CollectionError.MEMBER_ALREADY_EXISTS);
+		});
+	}
+}
