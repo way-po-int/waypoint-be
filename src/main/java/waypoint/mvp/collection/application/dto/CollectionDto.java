81255796b1d9d967592514cd05714fea81359bf4
Cori1304-Seong <andyjm7739@gmail.com>
2026-01-16 17:44:06 +0900

    feat(Collection): collection dto @Builder 제거 [WAPO-64]

--- src/main/java/waypoint/mvp/collection/application/dto/CollectionDto.java
+++ src/main/java/waypoint/mvp/collection/application/dto/CollectionDto.java
@@ -1,17 +1,12 @@
 package waypoint.mvp.collection.application.dto;
 
-import lombok.Builder;
 import waypoint.mvp.collection.domain.Collection;
 
-@Builder
 public record CollectionDto(
     Long id,
     String title
 ) {
     public static CollectionDto from(Collection collection) {
-        return CollectionDto.builder()
-            .id(collection.getId())
-            .title(collection.getTitle())
-            .build();
+        return new CollectionDto(collection.getId(), collection.getTitle());
     }
 }
--- src/main/java/waypoint/mvp/collection/presentation/dto/response/CollectionResponse.java
+++ src/main/java/waypoint/mvp/collection/presentation/dto/response/CollectionResponse.java
@@ -1,17 +1,12 @@
 package waypoint.mvp.collection.presentation.dto.response;
 
-import lombok.Builder;
 import waypoint.mvp.collection.application.dto.CollectionDto;
 
-@Builder
 public record CollectionResponse(
     Long id,
     String title
 ) {
     public static CollectionResponse from(CollectionDto dto) {
-        return CollectionResponse.builder()
-            .id(dto.id())
-            .title(dto.title())
-            .build();
+        return new CollectionResponse(dto.id(), dto.title());
     }
 }
