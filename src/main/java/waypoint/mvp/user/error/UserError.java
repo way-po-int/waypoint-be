59471f3f8e91b8768e1f8ead99af52cf8cb46904
Cori1304-Seong <andyjm7739@gmail.com>
2026-01-16 19:08:47 +0900

    refactor(UserError): 오타 수정 [WAPO-64]

--- src/main/java/waypoint/mvp/user/error/UserError.java
+++ src/main/java/waypoint/mvp/user/error/UserError.java
@@ -9,7 +9,7 @@ import waypoint.mvp.global.error.ErrorCode;
 @Getter
 @RequiredArgsConstructor
 public enum UserError implements ErrorCode {
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User를 찾을 수 업습니다.");
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
