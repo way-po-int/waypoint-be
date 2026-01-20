71fc63055f8bc70fb4c8dbe74a21567624af5293
jh <biruk23@naver.com>
2026-01-20 17:13:18 +0900

    feat(place): PlaceDetail에 필드 추가 [WAPO-72]

--- src/main/java/waypoint/mvp/place/domain/PlaceDetail.java
+++ src/main/java/waypoint/mvp/place/domain/PlaceDetail.java
@@ -17,14 +17,52 @@ public class PlaceDetail {
 	@Column(nullable = false, unique = true)
 	private String placeId;
 
+	@Column(length = 100)
+	private String primaryType;
+
+	@Column(length = 100)
+	private String primaryTypeDisplayName;
+
+	@Column(length = 2048)
+	private String googleMapsUri;
+
+	@Column(length = 512)
+	private String photoName;
+
 	@Builder(access = AccessLevel.PRIVATE)
-	private PlaceDetail(String placeId) {
+	private PlaceDetail(
+		String placeId,
+		String primaryType,
+		String primaryTypeDisplayName,
+		String googleMapsUri,
+		String photoName
+	) {
 		this.placeId = placeId;
+		this.primaryType = primaryType;
+		this.primaryTypeDisplayName = primaryTypeDisplayName;
+		this.googleMapsUri = googleMapsUri;
+		this.photoName = photoName;
 	}
 
 	public static PlaceDetail create(String placeId) {
 		return builder()
 			.placeId(placeId)
+			.build();
+	}
+
+	public static PlaceDetail create(
+		String placeId,
+		String primaryType,
+		String primaryTypeDisplayName,
+		String googleMapsUri,
+		String photoName
+	) {
+		return builder()
+			.placeId(placeId)
+			.primaryType(primaryType)
+			.primaryTypeDisplayName(primaryTypeDisplayName)
+			.googleMapsUri(googleMapsUri)
+			.photoName(photoName)
 			.build();
 	}
 }
