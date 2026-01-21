2753ab23559f690d59d2cd7719edce2a4a019e3f
Cori1304-Seong <andyjm7739@gmail.com>
2026-01-21 18:58:27 +0900

    feat(collection): 자신이 속한 collection가져오는 qury 추가 [WAPO-98]
    기존에 사용하던 로직은 자기가 속하지 않은 collection도 가져왔기에 자신이 속한 collection 가져오도록 수정합니다.

--- src/main/java/waypoint/mvp/collection/infrastructure/persistence/CollectionRepository.java
+++ src/main/java/waypoint/mvp/collection/infrastructure/persistence/CollectionRepository.java
@@ -1,7 +1,14 @@
 package waypoint.mvp.collection.infrastructure.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import waypoint.mvp.collection.domain.Collection;
 
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
+
+	@Query("SELECT c FROM Collection c WHERE c.id IN (SELECT cm.collection.id FROM CollectionMember cm WHERE cm.user.id = :userId)")
+	Page<Collection> findAllByUserId(Long userId, Pageable pageable);
+
 }
