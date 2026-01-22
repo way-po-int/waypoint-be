00392db905d1e98808b8bb13e9df530141f3c387
조윤성 <yunsung1206@gmail.com>
2026-01-22 14:36:17 +0900

    chore(place): Spring AI 의존성 추가 [WAPO-89]
    - google-genai

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation(platform(libs.spring.ai.bom))
+    implementation("org.springframework.ai:spring-ai-starter-model-google-genai")
     implementation("org.hibernate.orm:hibernate-spatial")
     implementation(libs.jjwt.api)
 
