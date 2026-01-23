8e0a02299a442850159bba08c574c2135c683762
조윤성 <yunsung1206@gmail.com>
2026-01-23 15:48:46 +0900

    feat(place): URL 미디어 타입 판별 로직 구현 [WAPO-89]
    - ExtractStatus 패키지 이동 (collection → place)

--- src/main/java/waypoint/mvp/place/domain/SocialMediaType.java
+++ src/main/java/waypoint/mvp/place/domain/SocialMediaType.java
@@ -1,6 +1,38 @@
 package waypoint.mvp.place.domain;
 
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import waypoint.mvp.global.error.exception.BusinessException;
+import waypoint.mvp.place.error.SocialMediaError;
+
+@Getter
+@RequiredArgsConstructor
 public enum SocialMediaType {
-	YOUTUBE,
-	YOUTUBE_SHORTS
+	YOUTUBE_SHORTS(List.of("youtube.com/shorts/")),
+	YOUTUBE(List.of("youtube.com/", "youtu.be/"));
+
+	private final List<String> supports;
+
+	public static SocialMediaType from(String url) {
+		try {
+			URI uri = URI.create(url);
+			String u = uri.getHost() + uri.getPath();
+			return Arrays.stream(values())
+				.filter(type -> type.isSupported(u))
+				.findFirst()
+				.orElseThrow();
+		} catch (IllegalArgumentException | NullPointerException | NoSuchElementException e) {
+			throw new BusinessException(SocialMediaError.UNSUPPORTED_SOCIAL_MEDIA);
+		}
+	}
+
+	private boolean isSupported(String url) {
+		return supports.stream()
+			.anyMatch(url::contains);
+	}
 }
