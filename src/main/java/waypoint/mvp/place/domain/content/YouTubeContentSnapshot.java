948c29441722a2c826efdc074aa16281f0989a8c
조윤성 <yunsung1206@gmail.com>
2026-01-27 17:50:42 +0900

    feat(place): YouTube 콘텐츠 정보 추출 로직 구현 [WAPO-89]
    - llm 입력으로 추출한 정보를 같이 전송

---
+++ src/main/java/waypoint/mvp/place/domain/content/YouTubeContentSnapshot.java
@@ -0,0 +1,21 @@
+package waypoint.mvp.place.domain.content;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class YouTubeContentSnapshot extends ContentSnapshot {
+
+	private String description;
+	private List<String> tags;
+	private String comment;
+	private String channelId;
+	private String channelTitle;
+	private String duration;
+}
