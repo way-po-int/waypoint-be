06f90a157737a02f192822d022d5977ba676b83d
jh <biruk23@naver.com>
2026-01-29 15:43:15 +0900

    refactor(collection): N+1 방지 위해 fetch join 적용 [WAPO-66]

--- src/main/java/waypoint/mvp/collection/infrastructure/persistence/CollectionPlacePreferenceRepository.java
+++ src/main/java/waypoint/mvp/collection/infrastructure/persistence/CollectionPlacePreferenceRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import waypoint.mvp.collection.domain.CollectionPlacePreference;
 
@@ -12,8 +14,10 @@ public interface CollectionPlacePreferenceRepository
 
 	Optional<CollectionPlacePreference> findByPlaceIdAndMemberId(Long collectionPlaceId, Long collectionMemberId);
 
-	List<CollectionPlacePreference> findAllByPlaceIdAndType(Long collectionPlaceId,
-		CollectionPlacePreference.Type type);
+	@Query("select p from CollectionPlacePreference p join fetch p.member where p.place.id = :collectionPlaceId and p.type = :type")
+	List<CollectionPlacePreference> findAllByPlaceIdAndType(@Param("collectionPlaceId") Long collectionPlaceId,
+		@Param("type") CollectionPlacePreference.Type type);
 
-	List<CollectionPlacePreference> findAllByPlaceIdIn(List<Long> collectionPlaceIds);
+	@Query("SELECT p FROM CollectionPlacePreference p JOIN FETCH p.member WHERE p.place.id IN :collectionPlaceIds")
+	List<CollectionPlacePreference> findAllByPlaceIdIn(@Param("collectionPlaceIds") List<Long> collectionPlaceIds);
 }
