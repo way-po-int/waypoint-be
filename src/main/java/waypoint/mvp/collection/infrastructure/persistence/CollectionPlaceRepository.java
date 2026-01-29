ac9c8baec137dce55faf073896b4fb620f4ff093
jh <biruk23@naver.com>
2026-01-29 15:43:15 +0900

    refactor(collection): N+1 방지 위해 fetch join 적용 [WAPO-66]

--- src/main/java/waypoint/mvp/collection/infrastructure/persistence/CollectionPlaceRepository.java
+++ src/main/java/waypoint/mvp/collection/infrastructure/persistence/CollectionPlaceRepository.java
@@ -5,16 +5,23 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import waypoint.mvp.collection.domain.CollectionPlace;
 
 public interface CollectionPlaceRepository extends JpaRepository<CollectionPlace, Long> {
-
-	Optional<CollectionPlace> findByIdAndCollectionId(Long id, Long collectionId);
+	@Query("SELECT cp FROM CollectionPlace cp LEFT JOIN FETCH cp.place LEFT JOIN FETCH cp.socialMedia WHERE cp.id = :id AND cp.collection.id = :collectionId")
+	Optional<CollectionPlace> findByIdAndCollectionId(@Param("id") Long id, @Param("collectionId") Long collectionId);
 
 	boolean existsByCollectionIdAndPlaceId(Long collectionId, Long placeId);
 
-	Page<CollectionPlace> findAllByCollectionId(Long collectionId, Pageable pageable);
+	@Query(value = "select cp from CollectionPlace cp join fetch cp.place where cp.collection.id = :collectionId",
+		countQuery = "select count(cp) from CollectionPlace cp where cp.collection.id = :collectionId")
+	Page<CollectionPlace> findAllByCollectionId(@Param("collectionId") Long collectionId, Pageable pageable);
 
-	Page<CollectionPlace> findAllByCollectionIdAndAddedByIdNot(Long collectionId, Long myMemberId, Pageable pageable);
+	@Query(value = "select cp from CollectionPlace cp join fetch cp.place where cp.collection.id = :collectionId and cp.addedBy.id <> :myMemberId",
+		countQuery = "select count(cp) from CollectionPlace cp where cp.collection.id = :collectionId and cp.addedBy.id <> :myMemberId")
+	Page<CollectionPlace> findAllByCollectionIdAndAddedByIdNot(@Param("collectionId") Long collectionId,
+		@Param("myMemberId") Long myMemberId, Pageable pageable);
 }
