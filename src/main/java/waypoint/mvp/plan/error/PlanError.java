be0be6a66a3ea115bf3c4f98077a72eb3e26488a
Cori1304-Seong <andyjm7739@gmail.com>
2026-01-31 03:34:14 +0900

    feat(plan): Plan_NOT_FOUND 에러 추가 [WAPO-113]

--- src/main/java/waypoint/mvp/plan/error/PlanError.java
+++ src/main/java/waypoint/mvp/plan/error/PlanError.java
@@ -9,6 +9,7 @@ import waypoint.mvp.global.error.ErrorCode;
 @Getter
 @RequiredArgsConstructor
 public enum PlanError implements ErrorCode {
+	Plan_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 플랜을 찾을 수 없습니다."),
 	INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "종료일은 시작일보다 빠를 수 없습니다.");
 
 	private final HttpStatus httpStatus;
