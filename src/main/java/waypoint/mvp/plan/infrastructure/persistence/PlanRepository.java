dd6df47be74a1cf4bcad6d2d7ffc456de7775771
Cori1304-Seong <andyjm7739@gmail.com>
2026-01-30 17:29:32 +0900

    feat(plan): 플랜 조회 기능 추가 [WAPO-113]

--- src/main/java/waypoint/mvp/plan/infrastructure/persistence/PlanRepository.java
+++ src/main/java/waypoint/mvp/plan/infrastructure/persistence/PlanRepository.java
@@ -1,9 +1,15 @@
 package waypoint.mvp.plan.infrastructure.persistence;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import waypoint.mvp.plan.domain.Plan;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
+	@Query("SELECT pm.plan FROM PlanMember  pm WHERE pm.user.id = :userId")
+	Slice<Plan> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
 }
