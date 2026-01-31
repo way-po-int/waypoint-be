8d9d7d11c63599f38bb2800af688175b9ad82b12
jh <biruk23@naver.com>
2026-01-31 20:46:38 +0900

    fix(collection): SocialMediaResponse에서 없는 title 필드 제거 [WAPO-66]

--- src/main/java/waypoint/mvp/collection/application/dto/response/SocialMediaResponse.java
+++ src/main/java/waypoint/mvp/collection/application/dto/response/SocialMediaResponse.java
@@ -5,14 +5,12 @@ import waypoint.mvp.place.domain.SocialMedia;
 public record SocialMediaResponse(
 	String mediaType,
 	String url,
-	String title,
 	String summary
 ) {
 	public static SocialMediaResponse from(SocialMedia socialMedia) {
 		return new SocialMediaResponse(
 			socialMedia.getType().name(),
 			socialMedia.getUrl(),
-			socialMedia.getTitle(),
 			socialMedia.getSummary()
 		);
 	}
