56899fc460d076ca375821e798733c6a0a16b99b
jh <biruk23@naver.com>
2026-01-31 20:20:49 +0900

    refactor(collection): CollectionMemberResponse DTO 재사용 [WAPO-66]

--- src/main/java/waypoint/mvp/collection/application/dto/response/PickPassResponse.java
+++ src/main/java/waypoint/mvp/collection/application/dto/response/PickPassResponse.java
@@ -3,12 +3,12 @@ package waypoint.mvp.collection.application.dto.response;
 import java.util.List;
 
 public record PickPassResponse(
-	List<PickPassMemberResponse> pickedMember,
-	List<PickPassMemberResponse> passedMember
+	List<CollectionMemberResponse> pickedMember,
+	List<CollectionMemberResponse> passedMember
 ) {
 	public static PickPassResponse of(
-		List<PickPassMemberResponse> pickedMember,
-		List<PickPassMemberResponse> passedMember
+		List<CollectionMemberResponse> pickedMember,
+		List<CollectionMemberResponse> passedMember
 	) {
 		return new PickPassResponse(pickedMember, passedMember);
 	}
