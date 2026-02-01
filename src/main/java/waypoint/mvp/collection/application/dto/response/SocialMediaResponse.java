25405679a4fbc3c825f1ecee4a7ba55b438eaf13
jh <biruk23@naver.com>
2026-02-01 21:24:39 +0900

    feat(collection): SocialMediaResponse에 snapshot title 포함 [WAPO-66]

--- src/main/java/waypoint/mvp/collection/application/dto/response/SocialMediaResponse.java
+++ src/main/java/waypoint/mvp/collection/application/dto/response/SocialMediaResponse.java
@@ -5,12 +5,14 @@ import waypoint.mvp.place.domain.SocialMedia;
 public record SocialMediaResponse(
 	String mediaType,
 	String url,
+	String title,
 	String summary
 ) {
 	public static SocialMediaResponse from(SocialMedia socialMedia) {
 		return new SocialMediaResponse(
 			socialMedia.getType().name(),
 			socialMedia.getUrl(),
+			socialMedia.getSnapshot().getTitle(),
 			socialMedia.getSummary()
 		);
 	}
