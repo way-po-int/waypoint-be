5afce88f798e7170ab49c712b9b60d27c0c316d5
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-02 00:52:54 +0900

    feat(plan): 에러코드 추가 [WAPO-114]

--- src/main/java/waypoint/mvp/plan/error/PlanError.java
+++ src/main/java/waypoint/mvp/plan/error/PlanError.java
@@ -13,7 +13,8 @@ public enum PlanError implements ErrorCode {
 	INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "종료일은 시작일보다 빠를 수 없습니다."),
 	FORBIDDEN_NOT_OWNER(HttpStatus.FORBIDDEN, "플랜의 소유자만 이 작업을 수행할 수 있습니다."),
 	MEMBER_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 플랜에 속한 멤버입니다."),
-	FORBIDDEN_NOT_MEMBER(HttpStatus.FORBIDDEN, "플랜의 멤버만 이 작업을 수행할 수 있습니다.");
+	FORBIDDEN_NOT_MEMBER(HttpStatus.FORBIDDEN, "플랜의 멤버만 이 작업을 수행할 수 있습니다."),
+	NEED_TO_DELEGATE_OWNERSHIP(HttpStatus.CONFLICT, "소유자는 소유권을 다른 멤버에게 위임해야 탈퇴할 수 있습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
