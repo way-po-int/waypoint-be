cc118632bf220cc2cc4b51c43f4fbe7e0e412463
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-04 14:23:53 +0900

    refactor(collection): ChangeCollectionOwnerRequest로 이름 변경 [WAPO-116]

--- src/main/java/waypoint/mvp/collection/application/dto/request/ChangeCollectionOwnerRequest.java
+++ src/main/java/waypoint/mvp/collection/application/dto/request/ChangeCollectionOwnerRequest.java
@@ -2,7 +2,7 @@ package waypoint.mvp.collection.application.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record ChangeOwnerRequest(
+public record ChangeCollectionOwnerRequest(
 	@NotBlank(message = "owner로 변경하기 위해서는 memberId가 필요합니다.")
 	String collectionMemberId) {
 
