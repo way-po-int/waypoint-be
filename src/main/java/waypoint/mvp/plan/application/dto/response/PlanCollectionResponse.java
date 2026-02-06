cee1ce9f375f681476ee0b2c0ae7de7e7afe8da2
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-07 01:14:58 +0900

    refactor(plan): addedBy로 필드명 변경 [WAPO-118]

--- src/main/java/waypoint/mvp/plan/application/dto/response/PlanCollectionResponse.java
+++ src/main/java/waypoint/mvp/plan/application/dto/response/PlanCollectionResponse.java
@@ -2,17 +2,23 @@ package waypoint.mvp.plan.application.dto.response;
 
 import waypoint.mvp.collection.domain.Collection;
 import waypoint.mvp.plan.domain.PlanCollection;
+import waypoint.mvp.plan.domain.PlanMember;
 
 public record PlanCollectionResponse(
 	String collectionId,
 	String title,
-	PlanMemberResponse planMember
+	AddedBy addedBy
 ) {
 
 	public static PlanCollectionResponse from(PlanCollection planCollection) {
 		Collection collection = planCollection.getCollection();
-		PlanMemberResponse member = PlanMemberResponse.from(planCollection.getMember());
+		PlanMember member = planCollection.getMember();
+		AddedBy addedBy = new AddedBy(member.getPicture(), member.getPicture());
 
-		return new PlanCollectionResponse(collection.getExternalId(), collection.getTitle(), member);
+		return new PlanCollectionResponse(collection.getExternalId(), collection.getTitle(), addedBy);
+	}
+
+	record AddedBy(String picture, String nickname) {
+
 	}
 }
