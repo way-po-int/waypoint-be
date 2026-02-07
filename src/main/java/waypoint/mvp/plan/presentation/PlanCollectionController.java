2e94ea192bca077afaa573c45d9839ef05374912
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-08 00:19:45 +0900

    feat(planCollection): 플랜에 연결된 컬렉션 조회 API 추가 [WAPO-119]

--- src/main/java/waypoint/mvp/plan/presentation/PlanCollectionController.java
+++ src/main/java/waypoint/mvp/plan/presentation/PlanCollectionController.java
@@ -1,9 +1,11 @@
 package waypoint.mvp.plan.presentation;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import waypoint.mvp.auth.security.principal.AuthPrincipal;
 import waypoint.mvp.auth.security.principal.UserPrincipal;
 import waypoint.mvp.global.auth.annotations.AuthLevel;
 import waypoint.mvp.global.auth.annotations.Authorize;
@@ -37,4 +40,16 @@ public class PlanCollectionController {
 		URI location = URI.create("/plans/" + planId + "/collections");
 		return ResponseEntity.created(location).body(response);
 	}
+
+	@Authorize(level = AuthLevel.GUEST_OR_MEMBER)
+	@GetMapping
+	public ResponseEntity<List<PlanCollectionResponse>> findConnectedCollections(
+		@PathVariable String planId,
+		@AuthenticationPrincipal AuthPrincipal user
+	) {
+		List<PlanCollectionResponse> response = planCollectionService.findPlanCollectionResponses(planId, user);
+
+		return ResponseEntity.ok(response);
+	}
+
 }
