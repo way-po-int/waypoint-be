8b7f1836b6d95d038b23a741536776b2b634797e
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-08 00:19:25 +0900

    feat(planCollection): PlanCollectionResponse.planMemberId 추가 [WAPO-119]

--- src/main/java/waypoint/mvp/plan/application/dto/response/PlanCollectionResponse.java
+++ src/main/java/waypoint/mvp/plan/application/dto/response/PlanCollectionResponse.java
@@ -13,11 +13,17 @@ public record PlanCollectionResponse(
 	public static PlanCollectionResponse from(PlanCollection planCollection) {
 		Collection collection = planCollection.getCollection();
 		PlanMember member = planCollection.getMember();
-		AddedBy addedBy = new AddedBy(member.getExternalId(), member.getPicture(), member.getNickname());
+		String planMemberId = member.getDeletedAt() != null ? member.getExternalId() : "";
+		AddedBy addedBy = new AddedBy(planMemberId, member.getPicture(), member.getNickname());
 
 		return new PlanCollectionResponse(collection.getExternalId(), collection.getTitle(), addedBy);
 	}
 
+	/**
+	 * 컬렉션에 플랜을 추가한 사용자 정보
+	 *
+	 * <p>탈퇴한 멤버의 id는 빈 문자열("")로 처리된다.</p>
+	 */
 	record AddedBy(String planMemberId, String picture, String nickname) {
 
 	}
