03ff29f7cd181964aae956602d7052bca5d6ebda
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-07 19:41:48 +0900

    feat(plancollection): PLAN_COLLECTION_NOT_FOUND 에러 코드 추가 [WAPO-119]

--- src/main/java/waypoint/mvp/plan/error/PlanCollectionError.java
+++ src/main/java/waypoint/mvp/plan/error/PlanCollectionError.java
@@ -10,7 +10,8 @@ import waypoint.mvp.global.error.ErrorCode;
 @RequiredArgsConstructor
 public enum PlanCollectionError implements ErrorCode {
 
-	PLANCOLLECTION_ALREADY_EXISTS(HttpStatus.CONFLICT, "해당 컬렉션은 이미 추가되었습니다");
+	PLAN_COLLECTION_ALREADY_EXISTS(HttpStatus.CONFLICT, "해당 컬렉션은 이미 추가되었습니다"),
+	PLAN_COLLECTION_NOT_FOUND(HttpStatus.NOT_FOUND, "플랜에 등록된 컬렉션을 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
