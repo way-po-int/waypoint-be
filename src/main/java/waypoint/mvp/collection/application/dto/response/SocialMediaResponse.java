4fb135f3f13847c67b5c332284f81dc9830fefef
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-12 15:24:45 +0900

    feat(collection) SocialMediaResponse에 socialMediaId& authorName 추가 [WAPO-127]

--- src/main/java/waypoint/mvp/collection/application/dto/response/SocialMediaResponse.java
+++ src/main/java/waypoint/mvp/collection/application/dto/response/SocialMediaResponse.java
@@ -1,18 +1,25 @@
 package waypoint.mvp.collection.application.dto.response;
 
 import waypoint.mvp.place.domain.SocialMedia;
+import waypoint.mvp.place.domain.content.ContentSnapshot;
 
 public record SocialMediaResponse(
+	String socialMediaId,
 	String mediaType,
 	String url,
+	String authorName,
 	String title,
 	String summary
 ) {
 	public static SocialMediaResponse from(SocialMedia socialMedia) {
+		ContentSnapshot contentSnapshot = socialMedia.getSnapshot();
+
 		return new SocialMediaResponse(
+			socialMedia.getExternalId(),
 			socialMedia.getType().name(),
 			socialMedia.getUrl(),
-			socialMedia.getSnapshot().getTitle(),
+			socialMedia.getSnapshot().getAuthorName(),
+			contentSnapshot.getTitle(),
 			socialMedia.getSummary()
 		);
 	}
