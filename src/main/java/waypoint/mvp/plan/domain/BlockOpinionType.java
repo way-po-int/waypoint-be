b7923387cf9441f3a12b5605aac6f3346b5693f9
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-12 16:33:05 +0900

    feat(plan): BlockOpinion 추가 [WAPO-152]

---
+++ src/main/java/waypoint/mvp/plan/domain/BlockOpinionType.java
@@ -0,0 +1,12 @@
+package waypoint.mvp.plan.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum BlockOpinionType {
+	POSITIVE,
+	NEUTRAL,
+	NEGATIVE;
+}
