eda77fbf88205025581aa49be54e86786dd6633a
jh <biruk23@naver.com>
2026-02-13 14:39:06 +0900

    feat(user): 지원하지 않는 이미지 형식에 대한 에러 코드 추가 [WAPO-149]

--- src/main/java/waypoint/mvp/user/error/UserError.java
+++ src/main/java/waypoint/mvp/user/error/UserError.java
@@ -9,7 +9,8 @@ import waypoint.mvp.global.error.ErrorCode;
 @Getter
 @RequiredArgsConstructor
 public enum UserError implements ErrorCode {
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User를 찾을 수 없습니다.");
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User를 찾을 수 없습니다."),
+	UNSUPPORTED_IMAGE_CONTENT_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 이미지 형식입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
