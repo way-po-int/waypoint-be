7652d1af3ed417e9661ba45221155c5522053e8a
jh <biruk23@naver.com>
2026-02-13 15:03:30 +0900

    fix: CI 환경 빌드 오류 해결 [WAPO-149]

--- src/main/java/waypoint/mvp/global/config/S3Config.java
+++ src/main/java/waypoint/mvp/global/config/S3Config.java
@@ -4,25 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
 
-	@Bean
+	@Bean(destroyMethod = "close")
 	public S3Presigner s3Presigner(
-		@Value("${aws.region}") String region,
-		@Value("${aws.credentials.access-key}") String accessKey,
-		@Value("${aws.credentials.secret-key}") String secretKey
+		@Value("${aws.region}") String region
 	) {
 		return S3Presigner.builder()
 			.region(Region.of(region))
-			.credentialsProvider(
-				StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey))
-			)
 			.build();
 	}
 }
