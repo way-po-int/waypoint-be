083175b4ec5d5f86b756cedd71964249daebd13b
jh <biruk23@naver.com>
2026-02-16 02:35:19 +0900

    fix(global): S3Client 타임아웃 설정 추가 [WAPO-150]

--- src/main/java/waypoint/mvp/global/config/S3Config.java
+++ src/main/java/waypoint/mvp/global/config/S3Config.java
@@ -1,9 +1,12 @@
 package waypoint.mvp.global.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -26,6 +29,10 @@ public class S3Config {
 	) {
 		return S3Client.builder()
 			.region(Region.of(region))
+			.overrideConfiguration(ClientOverrideConfiguration.builder()
+				.apiCallTimeout(Duration.ofSeconds(60))
+				.apiCallAttemptTimeout(Duration.ofSeconds(20))
+				.build())
 			.build();
 	}
 }
