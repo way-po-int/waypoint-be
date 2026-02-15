7c1171c68a823c3658e093b8dd1ddc871ba4595a
jh <biruk23@naver.com>
2026-02-15 15:10:16 +0900

    feat(global): S3 삭제 처리를 위한 S3Client 빈 추가 [WAPO-150]

--- src/main/java/waypoint/mvp/global/config/S3Config.java
+++ src/main/java/waypoint/mvp/global/config/S3Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -15,6 +16,15 @@ public class S3Config {
 		@Value("${aws.region}") String region
 	) {
 		return S3Presigner.builder()
+			.region(Region.of(region))
+			.build();
+	}
+
+	@Bean
+	public S3Client s3Client(
+		@Value("${aws.region}") String region
+	) {
+		return S3Client.builder()
 			.region(Region.of(region))
 			.build();
 	}
