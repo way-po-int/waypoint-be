4c1899b14a461df9e3679728fd4ae327ae0fc3fa
조윤성 <yunsung1206@gmail.com>
2026-02-18 13:46:42 +0900

    feat(plan): 블록 의견 조회 API 구현 [WAPO-163]

--- src/main/java/waypoint/mvp/plan/error/BlockOpinionError.java
+++ src/main/java/waypoint/mvp/plan/error/BlockOpinionError.java
@@ -10,6 +10,7 @@ import waypoint.mvp.global.error.ErrorCode;
 @RequiredArgsConstructor
 public enum BlockOpinionError implements ErrorCode {
 
+	BLOCK_OPINION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 블록에 대한 의견을 찾을 수 없습니다."),
 	BLOCK_OPINION_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 해당 블록에 대한 의견을 작성했습니다."),
 	BLOCK_NOT_PLACE_TYPE(HttpStatus.BAD_REQUEST, "장소 블록에만 의견을 등록할 수 있습니다.");
 
