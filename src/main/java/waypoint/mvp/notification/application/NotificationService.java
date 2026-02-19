c7d417c1f5aca6d3541372cc4b86127740e9ece8
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-20 01:45:40 +0900

    feat(notification): 알림 생성 로직 추가 [WAPO-208]

--- src/main/java/waypoint/mvp/notification/application/NotificationService.java
+++ src/main/java/waypoint/mvp/notification/application/NotificationService.java
@@ -1,5 +1,7 @@
 package waypoint.mvp.notification.application;
 
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,9 @@ import lombok.RequiredArgsConstructor;
 import waypoint.mvp.auth.security.principal.UserPrincipal;
 import waypoint.mvp.global.common.SliceResponse;
 import waypoint.mvp.notification.application.dto.response.NotificationResponse;
+import waypoint.mvp.notification.domain.Notification;
+import waypoint.mvp.notification.domain.NotificationEventType;
+import waypoint.mvp.notification.domain.NotificationReceiveUser;
 import waypoint.mvp.notification.infrastructure.NotificationReceiveUserRepository;
 import waypoint.mvp.notification.infrastructure.NotificationRepository;
 
@@ -28,4 +33,29 @@ public class NotificationService {
 		return SliceResponse.from(notifications);
 	}
 
+	@Transactional
+	public void createNotification(
+		Long resourceId,
+		NotificationEventType type,
+		String message,
+		String linkUrl,
+		Long sendUserId,
+		List<Long> receiveUserIds
+	) {
+		Notification notification = Notification.create(
+			resourceId,
+			type,
+			message,
+			linkUrl,
+			sendUserId
+		);
+
+		notificationRepository.save(notification);
+
+		List<NotificationReceiveUser> receiveUsers = receiveUserIds.stream()
+			.map(receiveUserId -> NotificationReceiveUser.create(notification, receiveUserId))
+			.toList();
+
+		receiveUserRepository.saveAll(receiveUsers);
+	}
 }
