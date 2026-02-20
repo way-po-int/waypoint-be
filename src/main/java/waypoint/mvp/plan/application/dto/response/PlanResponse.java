4717f010d69d13961e36fe6d097a37c2d6335c98
jh <biruk23@naver.com>
2026-02-21 03:39:16 +0900

    fix(plan): 플랜 응답 누락 필드 추가 [WAPO-198]

--- src/main/java/waypoint/mvp/plan/application/dto/response/PlanResponse.java
+++ src/main/java/waypoint/mvp/plan/application/dto/response/PlanResponse.java
@@ -1,16 +1,18 @@
 package waypoint.mvp.plan.application.dto.response;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import waypoint.mvp.plan.domain.Plan;
 
-// TODO API dog과 동일하게 변경
 public record PlanResponse(
 	String planId,
 
 	String title,
+
+	String thumbnail,
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	LocalDate startDate,
@@ -18,16 +20,25 @@ public record PlanResponse(
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	LocalDate endDate,
 
-	int memberCount
+	int durationDays,
+	int memberCount,
+	int collectionCount
 ) {
 
-	public static PlanResponse from(Plan plan) {
+	public static PlanResponse from(Plan plan, String thumbnail, int collectionCount) {
 		return new PlanResponse(
 			plan.getExternalId(),
 			plan.getTitle(),
+			thumbnail,
 			plan.getStartDate(),
 			plan.getEndDate(),
-			plan.getMemberCount()
+			calcDurationDays(plan.getStartDate(), plan.getEndDate()),
+			plan.getMemberCount(),
+			collectionCount
 		);
+	}
+
+	private static int calcDurationDays(LocalDate startDate, LocalDate endDate) {
+		return (int)ChronoUnit.DAYS.between(startDate, endDate) + 1;
 	}
 }
