1b589ceb41a9e2fc6ab196ca3ee6658fc7226647
jh <biruk23@naver.com>
2026-02-21 03:44:27 +0900

    fix(plancollection): 플랜 컬렉션 추가 요청 id → ids로 변경 [WAPO-198]

--- src/main/java/waypoint/mvp/plan/application/dto/request/CreatePlanCollectionRequest.java
+++ src/main/java/waypoint/mvp/plan/application/dto/request/CreatePlanCollectionRequest.java
@@ -1,10 +1,13 @@
 package waypoint.mvp.plan.application.dto.request;
 
+import java.util.List;
+
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 public record CreatePlanCollectionRequest(
-	@NotBlank(message = "collectionId는 필수 입니다.")
-	String collectionId
+	@NotEmpty(message = "collectionIds는 필수 입니다.")
+	List<@NotBlank(message = "collectionId는 필수 입니다.") String> collectionIds
 ) {
 
 }
