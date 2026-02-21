c1c6a92ceb9c9f2069d5642dd56480b39c64ff2f
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-21 17:43:03 +0900

    feat(plan): place_id로 후보지 생성 로직 추가 [WAPO-224]

--- src/main/java/waypoint/mvp/place/infrastructure/persistence/PlaceRepository.java
+++ src/main/java/waypoint/mvp/place/infrastructure/persistence/PlaceRepository.java
@@ -1,5 +1,6 @@
 package waypoint.mvp.place.infrastructure.persistence;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
 	Optional<Place> findByDetailPlaceId(String detailPlaceId);
 
 	Optional<Place> findByExternalId(String externalId);
+
+	List<Place> findAllByExternalIdIn(List<String> externalIds);
 }
