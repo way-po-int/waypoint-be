8e5a882ac5f5a64b8cfda8a11c892cbb00d56e18
Cori1304-Seong <andyjm7739@gmail.com>
2026-02-23 23:34:08 +0900

    feat(block): BlockError에 블록의 시간 겹치치는 예외 추가 [WAPO-233]

--- src/main/java/waypoint/mvp/plan/error/BlockError.java
+++ src/main/java/waypoint/mvp/plan/error/BlockError.java
@@ -14,7 +14,9 @@ public enum BlockError implements ErrorCode {
 	CANDIDATE_COUNT_INSUFFICIENT(HttpStatus.BAD_REQUEST, "장소가 1개뿐이라면 후보지 확정을 사용할 수 없습니다."),
 	NOT_SELECTED(HttpStatus.BAD_REQUEST, "확정된 후보가 없습니다."),
 	CANNOT_ADD_CANDIDATE_TO_FREE_BLOCK(HttpStatus.BAD_REQUEST, "자유 시간 블록에는 후보지를 추가할 수 없습니다."),
-	BLOCK_CREATE_TYPE_UNSUPPORTED(HttpStatus.BAD_REQUEST, "블록 생성은 %s 타입만 가능합니다.");
+	BLOCK_CREATE_TYPE_UNSUPPORTED(HttpStatus.BAD_REQUEST, "블록 생성은 %s 타입만 가능합니다."),
+	TIME_BLOCK_EXACT_DUPLICATE(HttpStatus.CONFLICT, "동일한 시간대에 이미 일정이 있어요, 후보지로 추가해 주세요"),
+	TIME_BLOCK_OVERLAP(HttpStatus.CONFLICT, "겹치는 일정이 있어요");
 
 	private final HttpStatus httpStatus;
 	private final String message;
